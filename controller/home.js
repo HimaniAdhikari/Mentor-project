d1f729943fc15bdbf83ba478992c7a34b0bb6246
Ayush Rawat <ayushrawat324@gmail.com>
2023-04-09 21:12:50 +0530

    add sem1 page

--- controller/home.js
+++ controller/home.js
@@ -1,19 +1,45 @@
-exports.getHomePage = (req,res,next)=>{
-    res.render("home/index", {path: "/", isAuthenticated: req.session.loggedIn, user:req.session.user});
+const User = require("../model/user");
+exports.getHomePage = (req, res, next) => {
+  User.countDocuments().then((numberOfUsers) => {
+    // console.log(numberOfUsers);
+    res.render("home/index", {
+      path: "/",
+      isAuthenticated: req.session.loggedIn,
+      user: req.session.user,
+      numOfUsers: numberOfUsers
+    });
+  });
 };
 
-exports.getCoursepage = (req,res,next)=>{
-    res.render("home/courses", {path: "/courses",  isAuthenticated: req.session.loggedIn,user:req.session.user, errorMessage: req.flash("error")});
+exports.getCoursepage = (req, res, next) => {
+  res.render("home/courses", {
+    path: "/courses",
+    isAuthenticated: req.session.loggedIn,
+    user: req.session.user,
+    errorMessage: req.flash("error"),
+  });
 };
 
-exports.getContactPage = (req,res,next)=>{
-    res.render("home/contact" ,{path: "/contact",  isAuthenticated: req.session.loggedIn, user:req.session.user});
+exports.getContactPage = (req, res, next) => {
+  res.render("home/contact", {
+    path: "/contact",
+    isAuthenticated: req.session.loggedIn,
+    user: req.session.user,
+  });
 };
 
-exports.getEventPage = (req,res,next)=>{
-    res.render("home/events", {path: "/events",  isAuthenticated: req.session.loggedIn,user:req.session.user});
+exports.getEventPage = (req, res, next) => {
+  res.render("home/events", {
+    path: "/events",
+    isAuthenticated: req.session.loggedIn,
+    user: req.session.user,
+  });
 };
 
-exports.getToolPage = (req,res,next)=>{
-    res.render("home/tools", {path: "/tools",  isAuthenticated: req.session.loggedIn,  user:req.session.user});
+exports.getToolPage = (req, res, next) => {
+  res.render("home/tools", {
+    path: "/tools",
+    isAuthenticated: req.session.loggedIn,
+    user: req.session.user,
+  });
 };
