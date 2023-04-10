3da1346f1b0ba81dfbf26d00038f72bb36578ab7
Ayush Rawat <ayushrawat324@gmail.com>
2023-04-10 12:17:25 +0530

    admin.js update

--- controller/admin.js
+++ controller/admin.js
@@ -1,4 +1,4 @@
 
 exports.getAddNotesPage = (req,res,next) =>{
-    res.render("admin/add-notes",{path:'admin/add-notes', isAuthenticated: req.session.loggedIn, email:req.session.user.email});
+    res.render("admin/add-notes",{path:'admin/add-notes', isAuthenticated: req.session.loggedIn, user:req.session.user});
 }
