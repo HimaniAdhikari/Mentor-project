101a20edfac8222145cdcf897bd55d86f27db69f
Ayush Rawat <ayushrawat324@gmail.com>
2023-04-16 23:19:17 +0530

    add semester page , tool, events

--- controller/CSEsemester.js
+++ controller/CSEsemester.js
@@ -1,7 +1,28 @@
 exports.getSem1 = (req,res,next)=>{
     res.render("semesters/CSEsem/sem1", {path:"/courses", isAuthenticated: req.session.loggedIn, user: req.session.user});
 }
+exports.getSem2 = (req,res,next)=>{
+    res.render("semesters/CSEsem/sem2", {path:"/courses", isAuthenticated: req.session.loggedIn, user: req.session.user});
+}
+exports.getSem3 = (req,res,next)=>{
+    res.render("semesters/CSEsem/sem3", {path:"/courses", isAuthenticated: req.session.loggedIn, user: req.session.user});
+}
 
-exports.postDownloadNotes = (req,res,next)=>{
-    
-};
+exports.getSem4 = (req,res,next)=>{
+    res.render("semesters/CSEsem/sem4", {path:"/courses", isAuthenticated: req.session.loggedIn, user: req.session.user});
+}
+exports.getSem5 = (req,res,next)=>{
+    res.render("semesters/CSEsem/sem5", {path:"/courses", isAuthenticated: req.session.loggedIn, user: req.session.user});
+}
+exports.getSem6 = (req,res,next)=>{
+    res.render("semesters/CSEsem/sem6", {path:"/courses", isAuthenticated: req.session.loggedIn, user: req.session.user});
+}
+exports.getSem7 = (req,res,next)=>{
+    res.render("semesters/CSEsem/sem7", {path:"/courses", isAuthenticated: req.session.loggedIn, user: req.session.user});
+}
+exports.getSem8 = (req,res,next)=>{
+    res.render("semesters/CSEsem/sem8", {path:"/courses", isAuthenticated: req.session.loggedIn, user: req.session.user});
+}
+
+
+
--- routes/CSEsemester.js
+++ routes/CSEsemester.js
@@ -5,4 +5,18 @@ const isAuth = require("../middleware/iaAuth");
 
 router.get("/sem1",isAuth, semController.getSem1);
 
+router.get("/sem2",isAuth, semController.getSem2);
+
+router.get("/sem3",isAuth, semController.getSem3);
+
+router.get("/sem4",isAuth, semController.getSem4);
+
+router.get("/sem5",isAuth, semController.getSem5);
+
+router.get("/sem6",isAuth, semController.getSem6);
+
+router.get("/sem7",isAuth, semController.getSem7);
+
+router.get("/sem8",isAuth, semController.getSem8);
+
 module.exports = router;
